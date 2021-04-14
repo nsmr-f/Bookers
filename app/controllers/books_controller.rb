f67c6c4dc24894bf1709753f848c233bd691dc94
nsmr-f <fk.nsmr.puu@gmail.com>
2021-04-14 14:35:00 +0000

    Add 3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,40 @@
 class BooksController < ApplicationController
-  def index
-    @book = Book.all
+  def top
   end
-
+  
   def show
     @book = Book.find(params[:id])
   end
-
-  def new
-    @book =Book.new
+  
+  def index
+    @books = Book.all
   end
 
-  def edit
+  def new
+    @book = Book.new
   end
   
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path
+    redirect_to book_path(book.id)
+  end
+
+  def edit
+     @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
   
   private
   def book_params
-     params.require(:book).permit(:title, :body)
+  params.require(:book).permit(:title, :body)
+  
+  
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ get 'top' => 'books#top'
+ post 'books' => 'books#create'
+ get 'books' => 'books#index'
+ get 'books/:id/' => 'books#show', as: 'show_book'
+ get 'book/:id/edit' => 'books#edit', as: 'edit_book'
+ patch 'books/:id' => 'books#update', as: 'update_book'
  resources :books
+ 
 end
