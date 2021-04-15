89509a84b301c5768d7691d47032f7c6aa6f41b3
nsmr-f <fk.nsmr.puu@gmail.com>
2021-04-15 12:50:45 +0000

    Add 3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,21 +3,20 @@ class BooksController < ApplicationController
   end
   
   def show
-    @book = Book.find(params[:id])
+  end
+  
+  def new
+    @book = Book.new
   end
   
   def index
     @books = Book.all
   end
-
-  def new
-    @book = Book.new
-  end
   
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(book.id)
+    redirect_to books_path
   end
 
   def edit
@@ -30,11 +29,12 @@ class BooksController < ApplicationController
     redirect_to book_path(book.id)
   end
   
+  
   private
   def book_params
-  params.require(:book).permit(:title, :body)
-  
-  
-  end
-  
+    params.require(:book).permit(:title, :body)
+end
+
+
+
 end
